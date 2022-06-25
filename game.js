0a9a70ed25e77db1202d840674b16919c0dae33b
OrugaHz <104004406+OrugaHz@users.noreply.github.com>
2022-06-25 21:45:18 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,90 @@
+const gameSummary = {
+    numbers: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0,
+}
+gameSummary.numbers
+
+const game = {
+    playerHand: '',
+    aiHand: '',
+}
+
+const hands = document.querySelectorAll('.select img');
+//Pierwsza funkcja
+
+function handSelection() {
+    game.playerHand = this.dataset.option
+    console.log(game.playerHand);
+    hands.forEach(hand => hand.style.boxShadow = "");
+
+    this.style.boxShadow = "0 0 2px 2px yellow";
+}
+
+// const handSelection = (e) => {
+//     console.log(e.currentTarget);
+// }
+hands.forEach(img => img.addEventListener('click', handSelection))
+
+function computerChoice() {
+    const aiHand = hands[Math.floor(Math.random() * 3)].dataset.option;
+    return aiHand;
+}
+
+function checkResult(player, ai) {
+    if (player === ai) {
+        return "draw"
+
+    } else if ((player === "papier" && ai === "kamień") || (player === "kamień" && ai === "nożyczki") || (player === "nożyczki" && ai === "papier")) {
+        return "win"
+
+    } else {
+        return "loss"
+    }
+}
+
+//publikacja wyniku
+
+function publishResult(player, ai, result) {
+    document.querySelector('[data-summary="your-choice"]').textContent = player;
+    document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+
+    document.querySelector('p.numbers span').textContent = ++gameSummary.numbers;
+
+    if (result === "win") {
+        document.querySelector('p.wins span').textContent = ++gameSummary.wins;
+
+        document.querySelector('[data-summary="who-win"]').textContent = "Wygrałeś!";
+        document.querySelector('[data-summary="who-win"]').style.color = "green";
+    } else if (result === "loss") {
+        document.querySelector('p.losses span').textContent = ++gameSummary.losses;
+
+        document.querySelector('[data-summary="who-win"]').textContent = "Komputer wygrał!";
+        document.querySelector('[data-summary="who-win"]').style.color = "red";
+    } else {
+        document.querySelector('p.draws span').textContent = ++gameSummary.draws;
+
+        document.querySelector('[data-summary="who-win"]').textContent = "Remis!";
+        document.querySelector('[data-summary="who-win"]').style.color = "gray";
+    }
+}
+
+function endGame() {
+    document.querySelector(`[data-option="${game.playerHand}"]`).style.boxShadow = "";
+    game.playerHand = "";
+}
+
+function startGame() {
+    if (!game.playerHand) {
+        return alert('Wybierz dłoń!')
+    };
+    game.aiHand = computerChoice();
+
+    const gameResult = checkResult(game.playerHand, game.aiHand);
+    console.log(gameResult);
+    publishResult(game.playerHand, game.aiHand, gameResult);
+    endGame()
+}
+
+document.querySelector('.start').addEventListener("click", startGame)
